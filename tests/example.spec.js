adbf2dbac6ab5da414ba4030b05d9cfadbb4e4fc
Volodymyr Fedchenko <emirwtf@gmail.com>
2025-04-02 22:48:57 +0300

    did not  had time for last task

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -40,7 +40,18 @@ test.describe('Add Track Using "+" Button', () => {
 });
 
 test.describe('Verify Total Duration of the Playlist in Seconds', () => {
+  
+  test.only("Test that the total duration of all tracks in 'Your Playlist' is accurately calculated and displayed in seconds", async ({page}) => {
+        
+    await page.locator("button").nth(0).click();
+    await page.locator("button").nth(1).click();
+    await page.locator("button").nth(2).click();
 
+    
+    console.log(page.locator('#playlist-duration'))
+    // await expect(page.getByTestId('playlist-duration')).toHaveText(635);
+
+  });
  
 
 });

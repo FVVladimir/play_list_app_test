b3f23f1d98a77dbc9bf66ab93fea97dc3b93b5e7
Volodymyr Fedchenko <emirwtf@gmail.com>
2025-04-02 20:47:30 +0300

    first test done

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -5,65 +5,28 @@ test.beforeEach(async ({ page }) => {
   await page.goto('https://vite-react-alpha-lemon.vercel.app/');
 });
 
-
-
 test.describe('Search Functionality', () => {
+  
+  test('Test the search input field for filtering the available tracks', async ({ page }) => {  
 
-  // test('has title', async ({ page }) => {
+    const inputField = page.getByLabel("Search");    
     
-  //   await page.goto('https://vite-react-alpha-lemon.vercel.app/');
-  
-  //   // Expect a title "to contain" a substring.
-  //   await expect(page).toHaveTitle(/Playwright/);
-  // });
-  
-  test('get started link', async ({ page }) => {
-  
-    // Click the get started link.
-    // await page.getByRole('link', { name: 'Get started' }).click();
-  
-    // Expects page to have a heading with the name of Installation.
-    await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+        await expect(inputField).toBeVisible();
+        await inputField.fill("summer");
+        await expect(page.getByText('Summer Breeze')).toBeVisible();
+        
   });
 
 });
-// test.describe('Add Track Using "+" Button', () => {
 
-//   test('has title', async ({ page }) => {
-//     await page.goto('https://vite-react-alpha-lemon.vercel.app/');
-  
-//     // Expect a title "to contain" a substring.
-//     await expect(page).toHaveTitle(/Playwright/);
-//   });
-  
-//   test('get started link', async ({ page }) => {
-//     await page.goto('https://playwright.dev/');
-  
-//     // Click the get started link.
-//     await page.getByRole('link', { name: 'Get started' }).click();
-  
-//     // Expects page to have a heading with the name of Installation.
-//     await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-//   });
+test.describe('Add Track Using "+" Button', () => {
 
-// });
-// test.describe('Verify Total Duration of the Playlist in Seconds', () => {
+  
 
-//   test('has title', async ({ page }) => {
-//     await page.goto('https://vite-react-alpha-lemon.vercel.app/');
-  
-//     // Expect a title "to contain" a substring.
-//     await expect(page).toHaveTitle(/Playwright/);
-//   });
-  
-//   test('get started link', async ({ page }) => {
-//     await page.goto('https://playwright.dev/');
-  
-//     // Click the get started link.
-//     await page.getByRole('link', { name: 'Get started' }).click();
-  
-//     // Expects page to have a heading with the name of Installation.
-//     await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-//   });
+});
 
-// });
+test.describe('Verify Total Duration of the Playlist in Seconds', () => {
+
+ 
+
+});

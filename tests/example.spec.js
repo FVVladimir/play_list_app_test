5b1cb08b5e21f9881c1bceef54a8d4f0f7dbc8c7
Volodymyr Fedchenko <emirwtf@gmail.com>
2025-04-02 18:53:57 +0300

    add comand for comfortible run according task

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,4 +1,4 @@
-// @ts-check
+
 import { test, expect } from '@playwright/test';
 
 test.beforeEach(async ({ page }) => {
@@ -9,21 +9,21 @@ test.beforeEach(async ({ page }) => {
 
 test.describe('Search Functionality', () => {
 
-  test('has title', async ({ page }) => {
-    await page.goto('https://vite-react-alpha-lemon.vercel.app/');
+  // test('has title', async ({ page }) => {
+    
+  //   await page.goto('https://vite-react-alpha-lemon.vercel.app/');
   
   //   // Expect a title "to contain" a substring.
   //   await expect(page).toHaveTitle(/Playwright/);
   // });
   
-  // test('get started link', async ({ page }) => {
-  //   await page.goto('https://playwright.dev/');
+  test('get started link', async ({ page }) => {
   
-  //   // Click the get started link.
-  //   await page.getByRole('link', { name: 'Get started' }).click();
+    // Click the get started link.
+    // await page.getByRole('link', { name: 'Get started' }).click();
   
-  //   // Expects page to have a heading with the name of Installation.
-  //   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+    // Expects page to have a heading with the name of Installation.
+    await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
   });
 
 });
